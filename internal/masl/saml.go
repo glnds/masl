38f0ab32d1222ecf0a056fc9fa27db59d3722787
Gert Leenders <leenders.gert@gmail.com>
2018-04-30 09:52:28 +0200

    Fixed dependencies with dep

--- internal/masl/saml.go
+++ internal/masl/saml.go
@@ -7,17 +7,19 @@ import (
 	"net/http/httputil"
 	"time"
 
-	"github.com/Sirupsen/logrus"
 	"fmt"
 	"os"
+
+	"github.com/Sirupsen/logrus"
 )
 
 const (
-	generateTokenAPI = "auth/oauth2/token"
-	samlAssertionAPI = "api/1/saml_assertion"
+	generateTokenAPI  = "auth/oauth2/token"
+	samlAssertionAPI  = "api/1/saml_assertion"
+	samlAssertionAPI2 = "api/1/saml_assertion"
 )
 
-// APITokenResponse represents the JSON response from the OneLogin Generate APITokenResponse REST call
+// APITokenResponse represents the OnleLogin Generate API Token reponse
 type APITokenResponse struct {
 	Status struct {
 		Error   bool   `json:"error"`
@@ -35,6 +37,7 @@ type APITokenResponse struct {
 	} `json:"data"`
 }
 
+// SAMLAssertionRequest represents the OnleLogin SAML Assertion request
 type SAMLAssertionRequest struct {
 	UsernameOrEmail string `json:"username_or_email"`
 	Password        string `json:"password"`
@@ -42,6 +45,7 @@ type SAMLAssertionRequest struct {
 	Subdomain       string `json:"subdomain"`
 }
 
+// SAMLAssertionResponse represents the OnleLogin SAML Assertion response
 type SAMLAssertionResponse struct {
 	Status struct {
 		Type    string `json:"type"`
@@ -66,6 +70,14 @@ type SAMLAssertionResponse struct {
 	} `json:"data"`
 }
 
+// VerifyMFARequest represents the OnleLogin Verify MFA request
+type VerifyMFARequest struct {
+	AppID      string `json:"app_id"`
+	OtpToken   string `json:"otp_token"`
+	DeviceID   string `json:"device_id"`
+	StateToken string `json:"state_token"`
+}
+
 var httpClient = &http.Client{Timeout: 10 * time.Second}
 
 func logRequest(log *logrus.Logger, req *http.Request) {
@@ -78,52 +90,50 @@ func logResponse(log *logrus.Logger, resp *http.Response) {
 	log.Debug(string(dump))
 }
 
-// GenerateToken: Call to https://developers.onelogin.com/api-docs/1/oauth20-tokens/generate-tokens
+// GenerateToken Call to https://developers.onelogin.com/api-docs/1/oauth20-tokens/generate-tokens
 // Generate an access token and refresh token to access onelogin's resource APIs.
 func GenerateToken(conf Config, log *logrus.Logger) string {
 
 	url := conf.BaseURL + generateTokenAPI
-
-	var jsonStr = []byte(`{"grant_type":"client_credentials"}`)
-	req, err := http.NewRequest("POST", url, bytes.NewBuffer(jsonStr))
-	if err != nil {
-		panic(err)
-	}
+	requestBody := []byte(`{"grant_type":"client_credentials"}`)
 	auth := "client_id:" + conf.ClientID + ",client_secret:" + conf.ClientSecret
-	req.Header.Set("Authorization", auth)
-	req.Header.Set("Content-Type", "application/json")
-	logRequest(log, req)
-
-	resp, err := httpClient.Do(req)
-	if err != nil {
-		log.Fatal(err)
-	}
-	defer resp.Body.Close()
-	logResponse(log, resp)
 
 	apiToken := APITokenResponse{}
-	json.NewDecoder(resp.Body).Decode(&apiToken)
+	httpRequest(url, auth, requestBody, log, &apiToken)
 
 	//TODO: being a bit optimistic here ;)
 	return apiToken.Data[0].AccessToken
 }
 
-// SAMLAssertion: Call to https://api.eu.onelogin.com/api/1/saml_assertion
+// SAMLAssertion Call to https://api.eu.onelogin.com/api/1/saml_assertion
 func SAMLAssertion(conf Config, log *logrus.Logger, password string, apiToken string) {
 
 	url := conf.BaseURL + samlAssertionAPI
-
 	requestBody, err := json.Marshal(SAMLAssertionRequest{
 		UsernameOrEmail: conf.Username,
 		Password:        password,
 		AppID:           conf.AppID,
 		Subdomain:       conf.Subdomain})
+	if err != nil {
+		log.Fatalln(err)
+	}
+	auth := "bearer:" + apiToken
 
-	req, err := http.NewRequest("POST", url, bytes.NewBuffer(requestBody))
+	samlAssertion := SAMLAssertionResponse{}
+	httpRequest(url, auth, requestBody, log, &samlAssertion)
+
+	if samlAssertion.Status.Code == 401 {
+		fmt.Print("Password doesn't match :(")
+		os.Exit(0)
+	}
+}
+
+func httpRequest(url string, auth string, jsonStr []byte, log *logrus.Logger, target interface{}) {
+
+	req, err := http.NewRequest("POST", url, bytes.NewBuffer(jsonStr))
 	if err != nil {
 		panic(err)
 	}
-	auth := "bearer:" + apiToken
 	req.Header.Set("Authorization", auth)
 	req.Header.Set("Content-Type", "application/json")
 	logRequest(log, req)
@@ -133,13 +143,8 @@ func SAMLAssertion(conf Config, log *logrus.Logger, password string, apiToken st
 		log.Fatal(err)
 	}
 	defer resp.Body.Close()
+
 	logResponse(log, resp)
 
-	samlAssertion := SAMLAssertionResponse{}
-	json.NewDecoder(resp.Body).Decode(&samlAssertion)
-
-	if samlAssertion.Status.Code == 401{
-		fmt.Print("Password doesn't match :(")
-		os.Exit(0)
-	}
+	json.NewDecoder(resp.Body).Decode(target)
 }
