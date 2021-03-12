036a3e78c12d90cd44b9179b63df851c2366c76d
Gert Leenders <leenders.gert@gmail.com>
2021-03-12 17:05:23 +0100

    Test github actions notifications

--- cmd/masl/masl.go
+++ cmd/masl/masl.go
@@ -39,7 +39,7 @@ func main() {
 		logger = masl.GetLogger("info")
 	}
 
-	logger.Info("------------------ w00t w00t masl for you!?  ------------------")
+	l ogger.Info("------------------ w00t w00t masl for you!?  ------------------")
 
 	flags := parseFlags(conf)
 	logger.Info("Parsed the commandline flags")
